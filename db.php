ea532aa954b33834ac3c13d432944cdcd5755d87
southerncashflowaustralia <southerncashflowaustralia@gmail.com>
2026-01-20 06:21:29 +0000

    Add files via upload
    Upload config folder

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+// config/db.php
+
+$host = getenv('DB_HOST');
+$db   = getenv('DB_DATABASE');
+$user = getenv('DB_USERNAME');
+$pass = getenv('DB_PASSWORD');
+$port = getenv('DB_PORT');
+
+try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db;";
+    $pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
